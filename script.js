a43151f1490720226fbb885fd849e8771e575022
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-22 13:32:34 -0300

    Added constants, EventListeners and variables

---
+++ script.js
@@ -0,0 +1,19 @@
+const download = document.querySelector(".download");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrContainer = document.querySelector("#qr-code");
+const qrText = document.querySelector(".qr-text");
+const shareBtn = document.querySelector(".share-btn");
+const sizes = document.querySelector(".sizes");
+
+dark.addEventListener("input", handleDarkColor);
+light.addEventListener("input", handleLightColor);
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("change", handleSize);
+shareBtn.addEventListener("click", handleShare);
+
+const defaultUrl = "https://youtube.com/@AsmrProg";
+let colorLight = "#fff",
+  colorDark = "#000",
+  text = defaultUrl,
+  size = 300;
